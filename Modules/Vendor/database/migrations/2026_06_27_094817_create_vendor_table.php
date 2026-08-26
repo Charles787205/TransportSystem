79f7087f3cf932942cbeb48136b5726e34db05d3
charles787205 <j.canedo.527294@umindanao.edu.ph>
2026-08-26 23:33:14 +0800

    fix(vendor): fix details card height, limit preview lists, and add driver index page

--- Modules/Vendor/database/migrations/2026_06_27_094817_create_vendor_table.php
+++ Modules/Vendor/database/migrations/2026_06_27_094817_create_vendor_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
     {
         Schema::create('vendors', function (Blueprint $table) {
             $table->id();
-            $table->string("name", 50)->unique();
-            $table->string("email", 50)->unique();
-            $table->string("phone_number")->unique();
+            $table->string('name', 50)->unique();
+            $table->string('email', 50);
+            $table->string('phone_number')->unique();
             $table->boolean('is_active')->default(true);
             $table->timestamps();
         });
